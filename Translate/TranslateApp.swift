a3a400c1ed568be999c7e536f758cf83b8f7b50d
WUYUEZONG <w.yzong@outlook.com>
2022-11-16 14:37:30 +0800

    Initial Commit

---
+++ Translate/TranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateApp.swift
+//  Translate
+//
+//  Created by mntechMac on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct TranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
